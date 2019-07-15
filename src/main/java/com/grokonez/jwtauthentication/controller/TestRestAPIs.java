7efeecfe05b485b90456a1409c4eeecf354cab31
Martín de la Cruz Delgado <martindelacruzdelgado83@gmail.com>
2019-07-15 19:31:56 +0200

    Update TestRestAPIs.java
    TFM2

--- src/main/java/com/grokonez/jwtauthentication/controller/TestRestAPIs.java
+++ src/main/java/com/grokonez/jwtauthentication/controller/TestRestAPIs.java
@@ -12,7 +12,7 @@ public class TestRestAPIs {
 	@GetMapping("/api/test/user")
 	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 	public String userAccess() {
-		return ">>> Hoy toca TFM";
+		return ">>> Hoy toca TFM 2";
 	}
 
 	@GetMapping("/api/test/pm")
